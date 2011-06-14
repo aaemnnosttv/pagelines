812385915904b313f90ebca8675e6171bf9659f2
Andrew Powers <andrew@pagelines.com>
2011-06-14 01:10:30 -0700

    Extension refinements, Todos, docs

--- includes/class.extension.php
+++ includes/class.extension.php
@@ -11,8 +11,7 @@
 
 class PageLinesExtension{
 	
-	function __contruct(){
-	}
+	function __contruct(){ }
 
 
 	/**
@@ -37,39 +36,63 @@ class PageLinesExtension{
 	 */
 	function pagelines_register_sections(){
 
-
-		// Simplify compicated variables
-
+		/*
+			TODO 
+				- this should be broken down into 2 functions
+					- 1 $this->scan_directory();
+					- 2 $this->register_files();
+		*/
 
 		$section_dirs = array( 
-			'child' => STYLESHEETPATH . '/sections/',	
-			'parent' => PL_SECTIONS
-
-			);
+			'child' 	=> STYLESHEETPATH . '/sections/',	
+			'parent' 	=> PL_SECTIONS
+		);
 
 		// check for cached array	
 		if ( !$sections = get_transient( 'pagelines_sections' ) ) {
 			foreach ( apply_filters( 'pagelines_sections_dirs', $section_dirs) as $type => $dir ) {
+				
+				// Recurse through directory, 
+				/*
+					TODO  define $type in documentation
+				*/
 				$sections[$type] = $this->pagelines_getsections( $dir, $type );
+				
+				// Set transient to prevent performance problems.
+				// TODO switch this to activation/deactivation interface
 				set_transient( 'pagelines_sections', $sections, apply_filters( 'pagelines_section_cache_timeout', 120 ) );
+				
 			}
 		}
 
 		// main array containing child and parent sections
 		$sections = apply_filters( 'pagelines_section_admin', $sections );
 
+		/*
+			TODO 
+				- Simon review changes below for enhancements to readability (Pro Tip)
+				- I got a non array warning on $type, fixed, but what's the issue?
+		*/
 		foreach ( $sections as $type ) {
 
-			foreach( $type as $section ) {
-				if ($section['depends'] != '') { // do we have a dependency?
-					if (isset( $sections['parent'][$section['depends']]['class']) && file_exists( $sections['parent'][$section['depends']]['filename'] ) ) {
-						pagelines_register_section( $sections['parent'][$section['depends']]['class'], $sections['parent'][$section['depends']]['folder'], $sections['parent'][$section['depends']]['filename'] );	
-					}
-				} else {
-					if ( $section['type'] == 'child') {
-						pagelines_register_section( $section['class'], $section['filename'], null, array('child' => true ) );
+			if(is_array($type)){
+				foreach( $type as $section ) {
+				
+					// consolidate array vars
+					$dep = $section['depends'];
+					$dep_file = $sections['parent'][$section['depends']]['filename'];
+					$dep_class = $sections['parent'][$section['depends']]['class'];
+					$dep_folder = $sections['parent'][$section['depends']]['folder'];
+				
+					if ($dep != '') { // do we have a dependency?
+						if (isset( $dep_class ) && file_exists( $dep_file ) ) 
+							pagelines_register_section( $dep_class, $dep_folder, $dep_file ); 
+					
 					} else {
-						pagelines_register_section( $section['class'], $section['folder'], $section['filename'] );
+						if ( $section['type'] == 'child')
+							pagelines_register_section( $section['class'], $section['filename'], null, array('child' => true ) );
+						else
+							pagelines_register_section( $section['class'], $section['folder'], $section['filename'] );
 					}
 				}
 			}
@@ -77,6 +100,7 @@ class PageLinesExtension{
 		pagelines_register_hook('pagelines_register_sections'); // Hook
 	}		
 	/**
+	 * 
 	 * Helper function 
 	 * Returns array of section files.
 	 * @return array of php files
@@ -84,7 +108,8 @@ class PageLinesExtension{
 	 **/
 	function pagelines_getsections( $dir, $type ) {
 
-		if ( is_child_theme() == false && $type == 'child' || ! is_dir($dir) ) return;
+		if ( is_child_theme() == false && $type == 'child' || ! is_dir($dir) ) 
+			return;
 
 		$sections = array();
 		$it = new RecursiveIteratorIterator(new RecursiveDirectoryIterator( $dir, RecursiveIteratorIterator::LEAVES_ONLY));
@@ -106,18 +131,6 @@ class PageLinesExtension{
 		return $sections;	
 	}	
 }
-/*
-	TODO 
-		- make more readable
-			- add inline docs
-			- add whitespace
-			- consolidate complicated variable names into simple ones
-*/
-
-// =================================================
-// = TODO - FUNCTIONS FOR CONSILIDATION INTO CLASS =
-// =================================================
-
 
 
 
