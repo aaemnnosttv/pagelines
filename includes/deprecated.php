1933adb3bfadbaa9f72a4ac62d53a2b53f804443
Simon Prosser <pross@pross.org.uk>
2011-06-15 17:21:04 +0100

    Better message

--- includes/deprecated.php
+++ includes/deprecated.php
@@ -14,6 +14,6 @@
  * @deprecated Sections are now autoloaded and registered by platform core.
  **/
 function pagelines_register_section() {
-	_deprecated_function( __FUNCTION__, '2.0', 'You no longer need to register sections, simply drop them into CHILDTHEME/sections/ folder.' );
+	_deprecated_function( __FUNCTION__, '2.0', 'the CHILDTHEME/sections/ folder' );
 	return;
 }
