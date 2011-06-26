fa4f8104eda6c189384653094699ade9579f65f8
Andrew Powers <andrew@pagelines.com>
2011-06-26 13:12:45 -0700

    metapanel

--- admin/class.options.metapanel.php
+++ admin/class.options.metapanel.php
@@ -23,46 +23,88 @@ class PageLinesMetaPanel {
 	function __construct( $settings = array() ) {
 
 		global $post; 
+		global $pagenow;
 		
-		// Setup post, not auto-set for single post pages
-		$post = (!isset($post) && isset($_GET['post'])) ? get_post($_GET['post'], 'object') : null;
+		/**
+		 * Only load on new post or post edit pages
+		 * Makes sure there are no errors outside it, and improves processing
+		 */
+		if($pagenow == 'post.php' || $pagenow == 'post-new.php'){
+	
+			/**
+			 * Single post pages have post as GET, not $post as object
+			 */
+			$post = (!isset($post) && isset($_GET['post'])) ? get_post($_GET['post'], 'object') : null;
+	
+			/**
+			 * Get current post type, set as GET on 'add new' pages
+			 */
+			$this->ptype = ( !isset($post) && isset($_GET['post_type']) ) 
+								? $_GET['post_type'] 
+								: ( isset($post) && isset($post->post_type) ? $post->post_type : 
+									($pagenow == 'post-new.php' ? 'post' : null));		
+							
 		
-		// Get current post type
-		$posttype = ( isset( $_GET['post'] ) && ! in_array( get_post_type( $_GET['post'] ), apply_filters( 'pagelines_meta_blacklist', $this->blacklist ) ) ) 
-						? array( 'post', 'page', get_post_type( $_GET['post'] ) ) 
-						: array( 'post', 'page' );
+			$this->page_for_posts = ( isset($post) && get_option( 'page_for_posts' ) === $post->ID ) ? true : false;			
+			
+
 		
-		$this->non_meta = (!isset($post) || get_option( 'page_for_posts' ) === $post->ID ) ? true : false;
-		
-		// Only run the metapanel if it should be on this post type
-		if(isset($post) && in_array($post->post_type, $posttype)){
-		
-			$this->ctemplate = ( isset($post) && 'page' == $post->post_type && 0 != count( get_page_templates() ) && !empty($post->page_template)) 
-			 					? $post->page_template
-								: ( $post->post_type == 'post' )
-									? 'Single Post' 		
-									: ( $this->non_meta ) 
-										? 'Non Meta Page'
-										: 'Default Template';
-										
-		}
-		
-		
+
 			$defaults = array(
 					'id' 		=> 'pagelines-metapanel',
-					'name' 		=> 'PageLines Meta - '.$this->ctemplate,
-					'posttype' 	=> $posttype,
+					'name' 		=> $this->get_the_title(),
+					'posttype' 	=> $this->get_the_post_types(),
 					'location' 	=> 'normal', 
 					'priority' 	=> 'low', 
 					'hide_tabs'	=> false
 				);
-		
+	
 			$this->settings = wp_parse_args($settings, $defaults); // settings for post type
-		
+	
 			$this->register_actions();
-		
+	
 			$this->hide_tabs = $this->settings['hide_tabs'];
+			
+		}
 		
+	}
+	
+	function get_the_post_types(){
+		$the_post_types = ( isset( $_GET['post'] ) && ! in_array( get_post_type( $_GET['post'] ), apply_filters( 'pagelines_meta_blacklist', $this->blacklist ) ) ) 
+						? array( 'post', 'page', get_post_type( $_GET['post'] ) ) 
+						: array( 'post', 'page' );
+		
+		return $the_post_types;
+	}
+	
+	function get_the_title(){
+		global $post;
+		$this->base_name = 'PageLines Meta Settings';
+		$name = $this->base_name;
+		
+		if($this->ptype == 'post' || $this->ptype == 'page'){
+			$current_template = (isset($post)) ? get_post_meta($post->ID, '_wp_page_template', true) : false;
+	
+			$this->page_templates = array_flip($this->get_page_templates());
+		
+			if(  $this->ptype == 'page' && $current_template && $current_template != 'default') {
+
+				if(isset($this->page_templates[$current_template]))
+					$slug = $this->page_templates[$current_template];
+		
+			}elseif(  $this->ptype == 'page' )
+				$slug = 'Default';
+			elseif( $this->ptype == 'post' )
+				$slug = 'Single Post';
+			elseif( $this->page_for_posts )
+				$slug = 'Blog Page';
+			else 
+				$slub = '';
+		
+			$name .= sprintf(' <small style="font-style:italic">(%s)</small>', $slug);
+		} 
+		
+		return $name;
 	}
 	
 	function register_tab( $option_settings = array(), $option_array = array(), $location = 'bottom') {
@@ -136,8 +178,10 @@ class PageLinesMetaPanel {
 		global $post_ID;  
 		
 		// if page doesn't support settings
-		if ( $this->non_meta )
-			$this->non_meta_template(); return;
+		if ( $this->page_for_posts ){
+			$this->non_meta_template(); 
+			return;
+		}
 		
 		$option_engine = new OptEngine( 'meta' );
 		
@@ -228,194 +272,44 @@ class PageLinesMetaPanel {
 		<?php endif;
 	}
 
-	function meta_option_engine($oid, $o){
-		
-		$defaults = array(
-			'type'			=> '',
-			'selectvalues' 	=> array(),
-			'version'		=> '', 
-			'where'			=> false, 
-			'size'			=> 'reg',
-			'title'			=> '',
-			'desc'			=> null,
-			'label'			=> '', 
-			'exp'			=> null, 	
-		);
-		
-		$o = wp_parse_args($o, $defaults);
-		
-		// Check if correct post type
-		
-			
-			// Test Version
-			if(VPRO || (!VPRO && $o['version'] != 'pro')){ ?>
-				
-				<div class="metapanel_option fix">
-					<div class="metapanel_option_title">
-						<div class="metatab_option_header"><?php echo $o['title']; ?></div>
-						<?php if(isset($o['desc'])):?><div class="metatab_option_subheader"><?php echo $o['desc']; ?></div><?php endif;?>
-					</div>
-					<?php if( isset($o['exp']) ): ?>
-						<div class="metapanel_option_exp">
-							<div class="metapanel_option_exp_pad">
-								<strong>More Info</strong><br/>
-							<?php echo $o['exp']; ?>
-							</div>
-						</div>
-					<?php endif;?>
-					<div class="metapanel_option_input <?php if( isset($o['exp']) ) echo 'input_with_exp'; ?>">
-						<div class="metapanel_option_input_pad" >
-							<?php $this->option_breaker($oid, $o); ?>
-						</div>
-					</div>	
-				</div>
-		<?php }
-					
-	}
 	
-	function option_breaker($oid, $o){
-		
-		switch ( $o['type'] ){
-		
-			case 'select' :
-				$this->_get_select_option($oid, $o);
-				break;
-			case 'select_count' :
-				$this->_get_select_count_option($oid, $o);
-				break;
-			case 'select_taxonomy' :
-				$this->_get_taxonomy_select($oid, $o);
-				break;
-			case 'select_menu' :
-				$this->_get_menu_select($oid, $o);
-				break;
-			case 'textarea' :
-				$this->_get_textarea_option($oid, $o);
-				break;
-			case 'text' :
-				$this->_get_text_option($oid, $o);
-				break;
-			case 'check' :
-				$this->_get_check_option($oid, $o);
-				break;
-			case 'image_upload' :
-				$this->_get_image_upload_option($oid, $o);
-				break;
-			default :
-				do_action( 'pagelines_metapanel_' . $o['type'] , $oid, $o);
-				break;
-		
+	
+	/**
+	 * This was taken from core WP because the function hasn't loaded yet, and isn't accessible.
+	 */
+	function get_page_templates() {
+		$themes = get_themes();
+		$theme = get_current_theme();
+		$templates = $themes[$theme]['Template Files'];
+		$page_templates = array();
+
+		if ( is_array( $templates ) ) {
+			$base = array( trailingslashit(get_template_directory()), trailingslashit(get_stylesheet_directory()) );
+
+			foreach ( $templates as $template ) {
+				$basename = str_replace($base, '', $template);
+
+				// don't allow template files in subdirectories
+				if ( false !== strpos($basename, '/') )
+					continue;
+
+				if ( 'functions.php' == $basename )
+					continue;
+
+				$template_data = implode( '', file( $template ));
+
+				$name = '';
+				if ( preg_match( '|Template Name:(.*)$|mi', $template_data, $name ) )
+					$name = _cleanup_header_comment($name[1]);
+
+				if ( !empty( $name ) ) {
+					$page_templates[trim( $name )] = $basename;
+				}
+			}
 		}
-		
+
+		return $page_templates;
 	}
-	
-	
-	function _get_check_option($oid, $o){ global $post_ID;?>
-			<?php if(isset($o['label'])):?><label class="metatext-label"><?php echo $o['label'];?></label><?php endif;?>
-			<input class="admin_checkbox" type="checkbox" id="<?php echo $oid;?>" name="<?php echo $oid;?>" <?php checked((bool) m_pagelines($oid, $post_ID)); ?> />
-<?php }
-	
-	function _get_text_option($oid, $o){ global $post_ID;?>
-			<?php if(isset($o['label'])):?><label class="metatext-label"><?php echo $o['label'];?></label><?php endif;?>
-			<input type="text" class="html-text <?php echo 'metatext-'.$o['size'];?>"  id="<?php echo $oid;?>" name="<?php echo $oid;?>" value="<?php em_pagelines($oid, $post_ID); ?>" />
-<?php }
-
-	function _get_textarea_option($oid, $o){ global $post_ID;?>
-			<?php if(isset($o['label'])):?><label class="metatext-label"><?php echo $o['label'];?></label><?php endif;?>
-			<textarea class="html-textarea <?php echo 'metatextarea-'.$o['size'];?>"  id="<?php echo $oid;?>" name="<?php echo $oid;?>" /><?php em_pagelines($oid, $post_ID); ?></textarea>
-	<?php }
-	
-	function _get_select_option($oid, $o){  global $post_ID;?>
-		<label class="context" for="<?php echo $oid;?>"><?php echo $o['label'];?></label>
-			<select id="<?php echo $oid;?>" name="<?php echo $oid;?>">
-				<option value="">&mdash;<?php _e("SELECT", 'pagelines');?>&mdash;</option>
-
-				<?php foreach($o['selectvalues'] as $sval => $sset):?>
-					<?php if($o['type']=='select_same'):?>
-							<option value="<?php echo $sset;?>" <?php if(get_pagelines_meta($oid, $post_ID)==$sset) echo 'selected';?>><?php echo $sset;?></option>
-					<?php elseif(is_array($sset)):
-						$disabled_option = (isset($sset['version']) && $sset['version'] == 'pro' && !VPRO) ? true : false;
-					?>
-						<option <?php if($disabled_option) echo 'disabled="disabled" class="disabled_option"';?> value="<?php echo $sval;?>" <?php if(get_pagelines_meta($oid, $post_ID)==$sval) echo 'selected';?>><?php echo $sset['name']; if($disabled_option) echo ' (pro)';?></option>
-					<?php else:?>
-							<option value="<?php echo $sval;?>" <?php if(get_pagelines_meta($oid, $post_ID)==$sval) echo 'selected';?>><?php echo $sset;?></option>
-					<?php endif;?>
-
-				<?php endforeach;?>
-			</select>
-
-		
-	<?php }
-	
-	function _get_select_count_option($oid, $o){  global $post_ID;?>
-		
-		<select id="<?php echo $oid;?>" name="<?php echo $oid;?>">
-			<option value="">&mdash;SELECT&mdash;</option>
-			<?php if(isset($o['count_start'])): $count_start = $o['count_start']; else: $count_start = 0; endif;?>
-			<?php for($i = $count_start; $i <= $o['count_number']; $i++):?>
-					<option value="<?php echo $i;?>" <?php selected($i, get_pagelines_meta($oid, $post_ID)); ?>><?php echo $i;?></option>
-			<?php endfor;?>
-		</select>
-	<?php }
-	
-	
-	function _get_taxonomy_select($oid, $o){ 
-		
-		global $post_ID;
-		$terms_array = get_terms( $o['taxonomy_id']); ?> 
-
-		<?php if(is_array($terms_array) && !empty($terms_array)):?>
-		
-				<select id="<?php echo $oid;?>" name="<?php echo $oid;?>">
-					<option value="">&mdash;<?php _e("SELECT", 'pagelines');?>&mdash;</option>
-					<?php foreach($terms_array as $term):?>
-						<option value="<?php echo $term->slug;?>" <?php if(get_pagelines_meta($oid, $post_ID)==$term->slug) echo 'selected';?>><?php echo $term->name; ?></option>
-					<?php endforeach;?>
-				</select>
-			
-		<?php else:?>
-			<div class="meta-message">No sets have been created and added to a post yet!</div>
-		<?php endif;?>
-		
-	<?php }
-	
-	function _get_image_upload_option($oid, $o){ 
-		
-		global $post_ID;
-		
-		?>
-		
-			<p>
-				<label class="context" for="<?php echo $oid;?>"><?php echo $o['label'];?></label>
-				<input class="regular-text uploaded_url" type="text" name="<?php echo $oid;?>" value="<?php em_pagelines($oid, $post_ID); ?>" /><br/><br/>
-
-
-				<span id="<?php echo $oid;?>" class="image_upload_button button">Upload Image</span>
-				<span title="<?php echo $oid;?>" id="<?php echo $oid;?>" class="image_reset_button button">Remove</span>
-				<input type="hidden" class="ajax_action_url" name="wp_ajax_action_url" value="<?php echo admin_url("admin-ajax.php"); ?>" />
-				<input type="hidden" class="image_preview_size" name="img_size_<?php echo $oid;?>" value="100"/>
-			</p>
-			<?php if(m_pagelines($oid, $post_ID)):?>
-				<img class="pagelines_image_preview" id="image_<?php echo $oid;?>" src="<?php em_pagelines($oid, $post_ID); ?>" style="max-width: 100px"/>
-			<?php endif;?>
-
-		
-	<?php }
-	
-	function _get_menu_select($oid, $o){ global $post_ID; ?>
-		
-		<?php if(isset($o['label'])):?><label class="metatext-label"><?php echo $o['label'];?></label><?php endif;?>
-		<select id="<?php echo $oid;?>" name="<?php echo $oid;?>">
-			<option value="" >&mdash;SELECT&mdash;</option>
-			<?php	$menus = wp_get_nav_menus( array('orderby' => 'name') );
-					foreach ( $menus as $menu )
-						printf( '<option value="%d" %s>%s</option>', $menu->term_id, selected($menu->term_id, get_pagelines_meta($oid, $post_ID)), esc_html( $menu->name ) );
-			?>
-		</select>
-		
-	
-		
-	<?php }
 	
 }
 /////// END OF MetaOptions CLASS ////////
