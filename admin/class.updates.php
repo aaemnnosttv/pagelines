4421bb976a1a7c3859268666a2b31654c83f3b2e
Simon Prosser <simon@pagelines.com>
2011-08-23 19:10:43 +0100

    Fancy updates tab improved. Removed Updates tab from main settings.

--- admin/class.updates.php
+++ admin/class.updates.php
@@ -29,7 +29,6 @@ class PageLinesUpdateCheck {
 
 		if ( is_multisite() && ! is_super_admin() )
 			return;
-		add_filter( 'pagelines_options_array', array(&$this,'pagelines_theme_update_tab') );
 		if ( get_pagelines_option('disable_updates') == true )
 			return;
 		add_action('admin_notices', array(&$this,'pagelines_theme_update_nag') );
@@ -38,44 +37,6 @@ class PageLinesUpdateCheck {
 		add_action('load-update.php', array(&$this,'pagelines_theme_clear_update_transient') );
 		add_action('load-themes.php', array(&$this,'pagelines_theme_clear_update_transient') );
 
-	}
-
-	/**
-	 * TODO Document!
-	 */
-	function pagelines_theme_update_tab( $option_array ) {
-
-		$updates_exp = ( is_array( $a = get_transient('pagelines-update-' . $this->theme ) ) && isset($a['package']) && $a['package'] !== 'bad' ) 
-							? 'Updates are properly configured.' 
-							: 'Please use your login credentials for <a href="http://www.pagelines.com/launchpad/member.php">LaunchPad</a>.<br /><strong>Not</strong> your WordPress login.';
-
-		$updates = array(
-					'credentials' => array(
-						'version'	=> 'pro',
-						'type'		=> 'text_multi',
-						'inputsize'	=> 'tiny',
-						'selectvalues'	=> array(
-							'lp_username'	=> array('inputlabel'=>'Launchpad Username', 'default'=> $this->username ),
-							'lp_password'	=> array('inputlabel'=>'Launchpad Password', 'default'=> $this->password ),
-						),
-						'title'		=> 'Configure automatic updates',
-						'shortexp'	=> 'Get the latest theme updates direct from PageLines.',
-						'exp'		=> $updates_exp
-					),
-					'disable_updates' => array(
-							'default'	=> true,
-							'type'		=> 'check',
-							'inputlabel'	=> 'Disable update system?',
-							'title'		=> '',
-							'shortexp'	=> '',
-							'exp'		=> 'Completely disables the update system (includes notifications).'
-					)
-
-		);				
-				
-		
-		$option_array['advanced'] = array_merge( $option_array['advanced'], $updates );
-		return $option_array;
 	}
 	
 	/**
@@ -124,8 +85,10 @@ class PageLinesUpdateCheck {
 
 			}
 		echo '<div id="update-nag">';
-		printf( '%s %s is available. <a href="%s">What\'s new</a>.', $this->theme, esc_html( $pagelines_update['new_version'] ), esc_url( $pagelines_update['changelog_url'] ) );
-		echo ( $pagelines_update['package'] != 'bad' ) ? sprintf( ' You should <a href="%s">update now</a>.', admin_url('update-core.php') ) : ' Please configure auto-updates under misc-settings.';
+		printf( '%s %s is available.', $this->theme, esc_html( $pagelines_update['new_version'] ) );
+		
+		printf( ' %s', ( $pagelines_update['package'] != 'bad' ) ? sprintf( 'You should <a href="%s">update now</a>.', admin_url('update-core.php') ) : sprintf( '<a href="%s">Click here</a> to setup your PageLines account.', admin_url('admin.php?page=pagelines_extend#Account') ) );
+
 		echo ( $pagelines_update['extra'] ) ? sprintf('<br />%s', $pagelines_update['extra'] ) : '';
 		echo '</div>';
 	}	
