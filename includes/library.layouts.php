0e528b02593ec62db0dbf021ac3936fc9ab6afe5
Andrew Powers <andrew@pagelines.com>
2011-08-30 16:29:59 -0700

    layout handling library

---
+++ includes/library.layouts.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Includes functions for use in layouts
+ * 
+ **/
+
+/**
+ * 
+ **/
+function grid_row_start( $count, $total_count, $perline, $class = ''){
+
+	
+	$grid_row_start = ( $row_count % $perline == 0 ) ? true : false;
+	
+
+}
+
+/**
+ * 
+ **/
+function grid_row_end( $count, $total_count, $perline){
+
+	
+		
+	
+
+}
