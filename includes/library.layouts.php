5408d0e54b25dd5c9250b138fa512ca89b1865ae
Andrew Powers <andrew@pagelines.com>
2011-08-30 17:06:53 -0700

    layout functions

--- includes/library.layouts.php
+++ includes/library.layouts.php
@@ -5,23 +5,27 @@
  **/
 
 /**
- * 
+ *  Returns true on the first element in a row of elements
  **/
-function grid_row_start( $count, $total_count, $perline, $class = ''){
+function grid_row_start( $count, $total_count, $perline){
 
+	$row_count = $count + ( $perline - 1 );
 	
 	$grid_row_start = ( $row_count % $perline == 0 ) ? true : false;
 	
+	return $grid_row_start;
 
 }
 
 /**
- * 
+ *  Returns false on the last element in a row of elements
  **/
 function grid_row_end( $count, $total_count, $perline){
 
 	
-		
+	$row_count = $count + ( $perline - 1 );
 	
-
+	$box_row_end = ( ( $row_count + 1 ) % $perline == 0 || $count == $total_count ) ? true : false;
+	
+	return $box_row_end;
 }
