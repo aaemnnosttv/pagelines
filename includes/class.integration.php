c467bf57910ebdb17d2bd51e40d7e058ffb5d855
Andrew Powers <andrew@pagelines.com>
2011-10-24 20:29:22 -0700

    working that global

--- includes/class.integration.php
+++ includes/class.integration.php
@@ -9,9 +9,16 @@ class PageLinesIntegration {
 	
 	public $lesscode = '';
 	
-	function __construct(){  
+	function __construct( $integration ){  
 		
 		// All of WordPress Initialization is done by this time
+		
+		
+		
+		$this->integration = $integration;
+		
+		global $pl_integration;
+		$pl_integration = $this->integration;
 		
 		add_filter('pagelines_lesscode', array(&$this, 'load_less'));
 		
@@ -108,53 +115,14 @@ class PageLinesIntegration {
 	
 }
 
-
-function pl_integration_parse( $args ) {
+function pl_is_integration(){
+	global $pl_integration;
 	
-	$defaults = array(
-		
-		'buffer'=>	'',
-		'area'	=>	'head',
-		'type'	=>	'css'
-	);
+	return (isset($pl_integration) && !$pl_integration) ? true : false;
+}
 
-	$args = wp_parse_args( $args, $defaults );
-		
-	if ( $args['area'] == 'head' && $args['buffer'] ) {
+function pl_get_integration(){
+	global $in_integration;
 	
-		switch( $args['type'] ) {
-			
-			case 'css':
-				preg_match_all( '#<link rel=[\'|"]stylesheet[\'|"].*\/>#', $args['buffer'], $styles );
-				preg_match_all( '#<style type=[\'|"]text\/css[\'|"].*<\/style>#ms', $args['buffer'], $xtra_styles );
-				$styles = array_merge( $styles[0], $xtra_styles[0] );
-				if ( is_array( $styles ) ) {
-					$css = '';
-					foreach( $styles as $style )
-						$css .= $style . "\n";
-					return $css;
-				}
-			break;
-			
-			case 'js':
-				preg_match_all( '#<script type=[\'|"]text\/javascript[\'|"].*<\/script>#', $args['buffer'], $js );
-				if( is_array( $js[0] ) ) {
-					$js_out = '';
-					foreach( $js[0] as $j )
-						$js_out .= $j . "\n";
-				return $js_out;
-				}
-			break;
-
-			case 'divs':
-				preg_match( '/<div.*>/ms',$args['buffer'], $divs );
-				return ( isset( $divs[0] ) ) ? $divs[0] : '';
-			break;
-
-			default:
-				return false;
-			break;
-		} // switch
-
-	} //end if
+	return (isset($pl_integration) && !$pl_integration) ? $pl_integration : false;
 }
