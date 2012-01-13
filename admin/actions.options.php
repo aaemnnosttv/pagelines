34856e37666d71bb73c15ce3442d0b3ed43452d0
Simon Prosser <simon@pagelines.com>
2012-01-13 21:48:37 +0000

    revert menu code, wrapper still in place.
    Signed-off-by: Simon Prosser <simon@pagelines.com>

--- admin/actions.options.php
+++ admin/actions.options.php
@@ -4,6 +4,8 @@
 // = Build PageLines Option Interface =
 // ====================================
 
+// Add our menus where they belong.
+add_action( 'admin_menu', 'pagelines_add_admin_menu' );
 
 add_action('admin_menu', 'pagelines_add_admin_menus');
 
@@ -36,6 +38,28 @@ function pagelines_insert_menu( $page_title, $menu_title, $capability, $menu_slu
 		
 		return add_theme_page( $page_title, sprintf( 'PageLines %s', $menu_title ), $capability, $menu_slug, $function );	
 	}
+}
+
+
+
+function pagelines_insert_menu_full( $page_title, $menu_title, $capability, $menu_slug, $function ) {
+	
+	return add_submenu_page( 'pagelines', $page_title, $menu_title, $capability, $menu_slug, $function );
+	
+}
+
+/**
+ * Full version menu wrapper.
+ * 
+ */
+function pagelines_add_admin_menu() {
+		global $menu;
+
+		// Create the new separator
+		$menu['2.995'] = array( '', 'edit_theme_options', 'separator-pagelines', '', 'wp-menu-separator' );
+
+		// Create the new top-level Menu
+		add_menu_page( 'Page Title', 'PageLines', 'edit_theme_options','pagelines', 'pagelines_build_option_interface', PL_ADMIN_IMAGES. '/favicon-pagelines.png', '2.996' );
 }
 
 
