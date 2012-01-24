8f5c34aee8e43eca156bca832bec8dfaeb991eca
Simon Prosser <simon@pagelines.com>
2012-01-24 13:08:12 +0000

    include Facebook js
    Signed-off-by: Simon Prosser <simon@pagelines.com>

--- sections/sharebar/section.php
+++ sections/sharebar/section.php
@@ -36,6 +36,16 @@ class PageLinesShareBar extends PageLinesSection {
 				
 				if(ploption('share_facebook')):
 					// Facebook
+					?>
+					<script>(function(d, s, id) {
+  							var js, fjs = d.getElementsByTagName(s)[0];
+  							if (d.getElementById(id)) return;
+  							js = d.createElement(s); js.id = id;
+  							js.src = "//connect.facebook.net/en_GB/all.js#xfbml=1";
+  							fjs.parentNode.insertBefore(js, fjs);
+							}(document, 'script', 'facebook-jssdk'));
+					</script>
+					<?php
 					printf('<div class="fb-like" data-href="%s" data-send="false" data-layout="button_count" data-width="90" data-show-faces="false" data-font="arial" style="vertical-align: top"></div>', $perm);
 				
 				endif;
