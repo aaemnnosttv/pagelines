d4962402c192624547a0b1f40cf75532d247bfa6
Andrew Powers <andrew@pagelines.com>
2012-02-01 18:03:10 -0800

    New callout section options

--- sections/callout/section.php
+++ sections/callout/section.php
@@ -23,20 +23,11 @@ class PageLinesCallout extends PageLinesSection {
 						'title' 			=> $this->name.' Text',	
 						'selectvalues'	=> array(
 							'pagelines_callout_header'		=> array('inputlabel'=>'Callout Header', 'default'=> ''),
-							'pagelines_callout_subheader'	=> array('inputlabel'=>'Callout Text', 'default'=> ''),
 						),				
 						'shortexp' 			=> 'The text for the callout banner section',
 						'exp' 				=> 'This text will be used as the title/text for the callout section of the theme.'
 
-				),				
-				'pagelines_callout_image' => array(
-					'type' 			=> 'image_upload',
-					'imagepreview' 	=> '270',
-					'inputlabel' 	=> 'Upload custom image',
-					'title' 		=> $this->name.' Image',						
-					'shortexp' 		=> 'Input Full URL to your custom header or logo image.',
-					'exp' 			=> 'Replaces the default callout image.'
-					),
+				),
 				'pagelines_callout_link' => array(
 					'type' => 'text',
 					'inputlabel' => 'Enter the link destination (URL)',
@@ -44,7 +35,34 @@ class PageLinesCallout extends PageLinesSection {
 					'shortexp' => 'The link destination of callout banner section',
 					'exp' => 'This URL will be used as the link for the callout section of the theme.'
 
-					)
+				),
+				'pagelines_callout_action_text' => array(
+					'type' 			=> 'text',
+					'inputlabel' 	=> 'Enter Button Text',
+					'title' 		=> 'Callout Action Text',						
+					'shortexp' 		=> 'Enter text for the callout button',
+					'exp' 			=> 'If you are not using an image as the action, CSS and a button with *this text* will be used.'
+				),
+								
+				'pagelines_callout_image' => array(
+					'type' 			=> 'image_upload',
+					'imagepreview' 	=> '270',
+					'inputlabel' 	=> 'Upload custom image',
+					'title' 		=> $this->name.' Image',						
+					'shortexp' 		=> 'Input Full URL to your custom header or logo image.',
+					'exp' 			=> 'Overrides the button output with a custom image.'
+				),
+				'pagelines_callout_align' => array(
+					'type' 			=> 'select',
+					'inputlabel' 	=> 'Select Alignment',
+					'title' 		=> 'Callout Alignment',			
+					'shortexp' 		=> 'Aligns the action left or right (defaults right)',
+					'exp' 			=> 'Default alignment for the callout is on the right.', 
+					'selectvalues'	=> array(
+						'right'		=> array('name'=>'Align Right'),
+						'left'		=> array('name '=>'Align Left'),
+					),
+				),
 			);
 
 			$metatab_settings = array(
@@ -62,27 +80,43 @@ class PageLinesCallout extends PageLinesSection {
 		
  	function section_template() {
 		$call_title = ploption('pagelines_callout_header', $this->oset);
-		$call_sub = ploption('pagelines_callout_subheader', $this->oset);
 		$call_img = ploption('pagelines_callout_image', $this->oset);
 		$call_link = ploption('pagelines_callout_link', $this->oset);
 		
+		$call_align = (ploption('pagelines_callout_align', $this->oset) == 'left') ? '' : 'rtimg';
+		
+		$call_action_text = (ploption('pagelines_callout_action_text', $this->oset)) ? ploption('pagelines_callout_action_text', $this->oset) : __('Start Here', 'pagelines');
+		
 		
 		if($call_title || $call_img){ ?>
-	<div class="callout-area fix">
-		<div class="callout_text">
+	<div class="callout-area media fix">
+		<?php if($call_link){ ?>
+		<div class="callout_action img <?php echo $call_align;?>">
+			<?php 
+			
+			
+				if( $call_img ){
+					printf('<div class="callout_image"><a href="%s" ><img src="%s" /></a></div>', $call_link, $call_img);
+				} else {
+					printf('<a class="button callout_button" href="%s">%s</a>', $call_link, $call_action_text);
+				}
+				
+				
+			?>
+		</div>
+		<?php } ?>
+		<div class="callout_text bd">
 			<div class="callout_text-pad">
 				<?php 
 				
 					printf( '<h2 class="callout_head %s">%s</h2>', (!$call_img) ? 'noimage' : '', $call_title);
-					printf( '<div class="callout_copy subhead">%s</div>', $call_sub);
 				
 				?>
+				
 			</div>
 		</div>
 		
-		<?php if( $call_img )
-				printf('<div class="callout_image"><a href="%s" ><img src="%s" /></a></div>', $call_link, $call_img);
-		?>
+		
 	</div>
 <?php
 
