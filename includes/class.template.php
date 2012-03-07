8f0ab2501d008706430237c93ff79e33748c43b8
Simon Prosser <simon@pagelines.com>
2012-03-07 19:02:41 +0000

    Add pagination to all archive types, posts/tags/cats/search
    Signed-off-by: Simon Prosser <simon@pagelines.com>

--- includes/class.template.php
+++ includes/class.template.php
@@ -1189,31 +1189,31 @@ function the_sub_templates( $t = 'templates' ){
 			),
 		'tag' => array(
 				'name'			=> __( 'Tag', 'pagelines' ),
-				'sections' 		=> ($t == 'main') ? array( 'PageLinesPostsInfo', 'PageLinesPostLoop' ) : array('PageLinesContent'),
+				'sections' 		=> ($t == 'main') ? array( 'PageLinesPostsInfo', 'PageLinesPostLoop', 'PageLinesPagination' ) : array('PageLinesContent'),
 				'version'		=> 'pro',
 				'page_type'		=> 'special'
 			),
 		'archive' => 	array(
 				'name'			=> __( 'Archive', 'pagelines' ),
-				'sections' 		=> ($t == 'main') ? array( 'PageLinesPostsInfo', 'PageLinesPostLoop' ) : array('PageLinesContent'),
+				'sections' 		=> ($t == 'main') ? array( 'PageLinesPostsInfo', 'PageLinesPostLoop', 'PageLinesPagination' ) : array('PageLinesContent'),
 				'version'		=> 'pro',
 				'page_type'		=> 'special'
 			),
 		'category' => 	array(
 				'name'			=> __( 'Category', 'pagelines' ),
-				'sections' 		=> ($t == 'main') ? array( 'PageLinesPostsInfo', 'PageLinesPostLoop' ) : array('PageLinesContent'),
+				'sections' 		=> ($t == 'main') ? array( 'PageLinesPostsInfo', 'PageLinesPostLoop', 'PageLinesPagination' ) : array('PageLinesContent'),
 				'version'		=> 'pro',
 				'page_type'		=> 'special'
 			),
 		'search' => 	array(
 				'name'			=> __( 'Search', 'pagelines' ),
-				'sections' 		=> ($t == 'main') ? array( 'PageLinesPostsInfo', 'PageLinesPostLoop' ) : array('PageLinesContent'),
+				'sections' 		=> ($t == 'main') ? array( 'PageLinesPostsInfo', 'PageLinesPostLoop', 'PageLinesPagination' ) : array('PageLinesContent'),
 				'version'		=> 'pro',
 				'page_type'		=> 'special'
 			),
 		'author' => 	array(
 				'name'			=> __( 'Author', 'pagelines' ),
-				'sections' 		=> ($t == 'main') ? array( 'PageLinesPostsInfo', 'PageLinesPostLoop' ) : array('PageLinesContent'),
+				'sections' 		=> ($t == 'main') ? array( 'PageLinesPostsInfo', 'PageLinesPostLoop', 'PageLinesPagination' ) : array('PageLinesContent'),
 				'version'		=> 'pro',
 				'page_type'		=> 'special'
 			),
