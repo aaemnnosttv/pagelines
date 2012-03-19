630922b61b764f5f24244b7a7c8c5bdb4900f3ea
Edward Caissie <edward.caissie@gmail.com>
2012-03-19 16:10:42 -0400

    `pagelines_title_tag` and `pagelines_meta_title`  inline documentation updates

--- includes/library.templates.php
+++ includes/library.templates.php
@@ -296,14 +296,15 @@ function pagelines_runtime_supersize(){
 
 	
 /**
-*
-* @TODO do
-*
-*/
+ * PageLines Title Tag
+ *
+ * Checks for AIO or WPSEO functionality, if they both do not exist then this will generate appropriate text for the installation site's <Title> meta tag.
+ *
+ * @since   ...
+ *
+ * @internal filter pagelines_meta_title provided for over-writing the default title text.
+ */
 function pagelines_title_tag(){
-	/*
-		Title Metatag
-	*/
 	echo "\n<title>";
 
 	if ( !function_exists( 'aiosp_meta' ) && !function_exists( 'wpseo_get_value' ) ) {
