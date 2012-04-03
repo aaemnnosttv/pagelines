8349789c0671c061a7ee7b744a8ced6d14f29b80
Andrew Powers <andrew@pagelines.com>
2012-04-03 16:37:37 -0700

    Default Header changes

--- includes/class.template.php
+++ includes/class.template.php
@@ -1117,7 +1117,7 @@ function the_template_map() {
 		'hook' 			=> 'pagelines_header', 
 		'name'			=> __( 'Site Header', 'pagelines' ),
 		'markup'		=> 'content', 
-		'sections' 		=> array( 'PageLinesBranding' , 'PageLinesNav', 'PageLinesSecondNav' )
+		'sections' 		=> array( 'PageLinesSecondNav' )
 	);
 	
 	$template_map['footer'] = array(
