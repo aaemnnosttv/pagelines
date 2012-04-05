1f6403651a0d3316a43c7dbaceb9dba0d3f755a3
Andrew Powers <andrew@pagelines.com>
2012-04-04 18:48:30 -0700

    Masthead start

--- sections/masthead/section.php
+++ sections/masthead/section.php
@@ -5,7 +5,7 @@
 	Author URI: http://www.pagelines.com
 	Description: A responsive full width splash and text area. Great for getting big ideas across quickly.
 	Class Name: PLMasthead	
-	Workswith: header
+	Workswith: templates, main, header, morefoot
 */
 
 /**
@@ -15,9 +15,7 @@
  * @author PageLines
  */
 class PLMasthead extends PageLinesSection {
-
-	var $default_limit = 2;
-
+ 
 	/**
 	 * Load styles and scripts
 	 */
@@ -40,6 +38,40 @@ class PLMasthead extends PageLinesSection {
 	*/
    function section_template( $clone_id ) { 
 	?>
+	
+	<header class="jumbotron masthead">
+	  <div class="inner">
+	    <h1>PageLines Framework</h1>
+	    <p>A Responsive, Drag &amp; Drop Platform for Beautiful Websites</p>
+	    <p class="download-info">
+	      <a href="http://www.pagelines.com/pricing/" class="btn btn-primary btn-large">Signup and Download</a>
+	     
+	    </p>
+	  </div>
+
+	  <div class="bs-links">
+	    <ul class="quick-links">
+	      <li><a href="./upgrading.html">Upgrading from 1.4</a></li>
+	      <li><a href="https://github.com/twitter/bootstrap/zipball/master">Download with docs</a></li>
+	      <li><a href="https://github.com/twitter/bootstrap/issues?state=open">Submit issues</a></li>
+	      <li><a href="https://github.com/twitter/bootstrap/wiki">Roadmap and changelog</a></li>
+	    </ul>
+	    <ul class="quick-links">
+	      <li>
+	        <iframe class="github-btn" src="http://markdotto.github.com/github-buttons/github-btn.html?user=twitter&repo=bootstrap&type=watch&count=true" allowtransparency="true" frameborder="0" scrolling="0" width="112px" height="20px"></iframe>
+	      </li>
+	      <li>
+	        <iframe class="github-btn" src="http://markdotto.github.com/github-buttons/github-btn.html?user=twitter&repo=bootstrap&type=fork&count=true" allowtransparency="true" frameborder="0" scrolling="0" width="98px" height="20px"></iframe>
+	      </li>
+	      <li class="follow-btn">
+	        <a href="https://twitter.com/twbootstrap" class="twitter-follow-button" data-link-color="#0069D6" data-show-count="true">Follow @twbootstrap</a>
+	      </li>
+	      <li class="tweet-btn">
+	        <a href="https://twitter.com/share" class="twitter-share-button" data-url="http://twitter.github.com/bootstrap/" data-count="horizontal" data-via="twbootstrap" data-related="mdo:Creator of Twitter Bootstrap">Tweet</a>
+	      </li>
+	    </ul>
+	  </div>
+	</header>
 
 	
 		<?php 
