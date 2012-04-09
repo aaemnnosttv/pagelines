d4a26835da7722e42668735ebfa2971610263aed
Simon Prosser <simon@pagelines.com>
2012-04-09 21:09:54 +0100

    Upgrader…upgrades.
    Signed-off-by: Simon Prosser <simon@pagelines.com>

--- includes/library.upgrades.php
+++ includes/library.upgrades.php
@@ -1,7 +1,5 @@
 <?php
 
-$up = new PageLinesUpgradePaths;
-
 class PageLinesUpgradePaths {
 	
 
@@ -12,66 +10,78 @@ class PageLinesUpgradePaths {
 	*/
 	function __construct() {
 		
-			if ( ! VPRO && 'pagelines' == basename( get_bloginfo('url') ) ) {
-
-				delete_option( PAGELINES_SETTINGS );
-				delete_option( PAGELINES_TEMPLATE_MAP );
-				return;
-			}
-
-			/**
-			* Upgrade from Platform(pro) to PageLines and import settings.
-			*/
-			if ( !is_array( $a = get_option( PAGELINES_SETTINGS ) ) )
-				$this->upgrade();
-
-			/**
-			* Insure the correct default logo is being displayed after import.
-			*/			
-			if ( ! VPRO && 'logo-platform.png' == basename( $a['pagelines_custom_logo'] ) )
-				plupop( 'pagelines_custom_logo',  PL_IMAGES . '/logo.png' );
-
-
-			/**
-			* Fix broken repeated excerpt problem on pagelines.com
-			*/			
-			if ( ! VPRO && 'pagelines' == basename( get_bloginfo('url') ) ) {
-
-				if ( ! isset( $a['content_blog'] ) || true != $a['content_blog'] )
-					plupop( 'content_blog', true );
-
-				if ( ! isset( $a['content_blog'] ) || true == $a['excerpt_blog'] )
-					plupop( 'excerpt_blog', false );
-
-				/**
-				* Fix broken templates
-				*/		
-				$t = ( array ) get_option( PAGELINES_TEMPLATE_MAP );
-
-				if ( 'PageLinesQuickSlider' != $t['main']['templates']['posts']['sections'][0] ) {
-					array_unshift( $t['main']['templates']['posts']['sections'], 'PageLinesQuickSlider' );
-					update_option( PAGELINES_TEMPLATE_MAP, $t );
-				}
-			}	
+		if ( ! VPRO && 'pagelines' == basename( get_bloginfo('url') ) ) {
+			
+			update_option( PAGELINES_SETTINGS, pagelines_settings_defaults() );
+			update_option( PAGELINES_TEMPLATE_MAP, the_template_map() );
 		}
+			
+		/**
+		* Insure the correct default logo is being displayed after import.
+		*/			
+		if ( ! VPRO && 'logo-platform.png' == basename( ploption( 'pagelines_custom_logo' ) ) )
+			plupop( 'pagelines_custom_logo',  PL_IMAGES . '/logo.png' );
+
+
+		/**
+		* Fix broken repeated excerpt problem on pagelines.com
+		*/			
+		if ( ! VPRO && 'pagelines' == basename( get_bloginfo('url') ) ) {
+			
+			if ( ! isset( $a['content_blog'] ) || true != $a['content_blog'] )
+				plupop( 'content_blog', true );
+			
+			if ( ! isset( $a['content_blog'] ) || true == $a['excerpt_blog'] )
+				plupop( 'excerpt_blog', false );
+			
+			/**
+			* Fix broken templates
+			*/		
+			$t = ( array ) get_option( PAGELINES_TEMPLATE_MAP, the_template_map() );
+
+			if ( 'PageLinesQuickSlider' != $t['main']['templates']['posts']['sections'][0] ) {
+				array_unshift( $t['main']['templates']['posts']['sections'], 'PageLinesQuickSlider' );
+				update_option( PAGELINES_TEMPLATE_MAP, $t );
+			}
+			
+			plupop( 'pagelines_version', CORE_VERSION );
+		}
+		/**
+		* Upgrade from Platform(pro) to PageLines and import settings.
+		*/
+		
+		$pagelines = get_option( PAGELINES_SETTINGS );
+		$platform = get_option( PAGELINES_SETTINGS_LEGACY );
+		
+		if ( is_array( $pagelines ) ) {
+			
+			if ( ! isset( $settings['pagelines_version'] ) )
+				plupop( 'pagelines_version', CORE_VERSION );
+			
+			// were done.
+			return;
+		}
+		
+		if ( is_array( $platform ) ) {
+			
+			$this->upgrade( $platform );			
+		}
+	}
 
 	/**
 	*
 	* @TODO document
 	*
 	*/
-	function upgrade() {
+	function upgrade( $settings ) {
 
-		if ( is_array( $settings = get_option( PAGELINES_SETTINGS_LEGACY ) ) ) {
 		
 			// beta versions will all be using the old array...
 			if ( isset( $settings['pl_login_image']) )
 				$this->beta_upgrade( $settings );
 			else 
 				$this->full_upgrade( $settings );
-		}
 	}
-
 
 	/**
 	*
@@ -98,8 +108,8 @@ class PageLinesUpgradePaths {
 					plupop( $key, $data );
 			}
 		}
+		plupop( 'pagelines_version', CORE_VERSION );
 	}
-	
 
 	/**
 	*
@@ -109,7 +119,8 @@ class PageLinesUpgradePaths {
 	function beta_upgrade( $settings ) {
 		
 		update_option( PAGELINES_SETTINGS, $settings );
-		update_option( PAGELINES_TEMPLATE_MAP, get_option( PAGELINES_TEMPLATE_MAP_LEGACY ) );
-		
+		update_option( PAGELINES_TEMPLATE_MAP, get_option( PAGELINES_TEMPLATE_MAP_LEGACY ) );		
 	}		
 }
+
+new PageLinesUpgradePaths;
