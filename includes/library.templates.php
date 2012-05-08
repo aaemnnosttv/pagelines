5dce586fe105c759ac2e2aadf7569358df63cb4c
Simon Prosser <simon@pagelines.com>
2012-05-09 00:01:23 +0100

    re-enable objects.less
    Signed-off-by: Simon Prosser <simon@pagelines.com>

--- includes/library.templates.php
+++ includes/library.templates.php
@@ -182,7 +182,7 @@ function pagelines_head_common(){
 	if(!has_action('override_pagelines_css_output')){	
 
 		// Get CSS Objects & Grids
-		pagelines_load_css_relative('css/objects.css', 'pagelines-objects');
+//		pagelines_load_css_relative('css/objects.css', 'pagelines-objects');
 		
 		// CSS Animations
 //		wp_enqueue_style('animate', PL_CSS.'/animate.css'); TODO do we need this?
