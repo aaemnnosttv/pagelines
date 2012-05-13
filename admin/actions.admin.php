a9bc93bb4374fe678abfba549c70cf91370280a3
Simon Prosser <simon@pagelines.com>
2012-05-13 02:35:52 +0100

    only admins flush caches
    Signed-off-by: Simon Prosser <simon@pagelines.com>

--- admin/actions.admin.php
+++ admin/actions.admin.php
@@ -328,9 +328,10 @@ new Store_RSS;
 
 add_action( 'admin_init', 'pagelines_set_versions' );
 function pagelines_set_versions() {
-	
-	delete_transient( 'pagelines_sections_cache' );
-	PageLinesRenderCSS::flush_version( false );
+	if ( current_user_can( 'edit_themes' ) ) {
+		delete_transient( 'pagelines_sections_cache' );
+		PageLinesRenderCSS::flush_version( false );
+	}
 	set_theme_mod( 'pagelines_version', pl_get_theme_data( get_template_directory(), 'Version' ) );
 	set_theme_mod( 'pagelines_child_version', pl_get_theme_data( get_stylesheet_directory(), 'Version' ) );
 }
