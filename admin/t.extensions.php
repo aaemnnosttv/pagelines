b485bc1567db1b39ad5ce00c0ab069c92a9a0830
Simon Prosser <simon@pagelines.com>
2012-06-04 21:05:44 +0100

    remove test array
    Signed-off-by: Simon Prosser <simon@pagelines.com>

--- admin/t.extensions.php
+++ admin/t.extensions.php
@@ -60,36 +60,4 @@ class PageLinesCoreExtensions {
 		
 		return apply_filters('pagelines_welcome_billboard', $bill);
 	}
-	
-	function test_array(){
-		
-		$data = array(
-			'story1'	=> array(
-				'title'		=> 'Rockin Theme', 
-				'text'		=> "It's time we introduce you to the first rule.  The first rule of PageLines is that you come first. We truly appreciate your business and support.", 
-				'img'		=> PARENT_URL . '/screenshot.png', 
-				'overview'	=> 'http://testlink.com/overview', 
-				'download'	=> 'http://testlink.com/download'
-			), 
-			'story3'	=> array(
-				'title'	=> 'BadAss Section', 
-				'text'	=> "Check out the <a href='".admin_url(PL_TEMPLATE_SETUP_URL)."'>Template Setup panel</a>! Using drag and drop you can completely control the appearance of your templates. Learn more in the <a href='http://www.pagelines.com/wiki/'>docs</a>.", 
-				'img'	=> PARENT_URL . '/screenshot.png', 
-				'overview'	=> 'http://testlink.com/overview', 
-				'download'	=> 'http://testlink.com/download'
-			),
-			'story4'	=> array(
-				'title'	=> 'Super Cool Plugin', 
-				'text'	=> "To maximize PageLines you're gonna need some extensions. Head over to the extensions page to get supported plugins and learn about extensions in the Store and Plus.", 
-				'img'	=> PARENT_URL . '/screenshot.png',
-				'overview'	=> 'http://testlink.com/overview', 
-				'download'	=> 'http://testlink.com/download'
-			),
-			
-		);
-		
-		return $data;
-		
-	}
-
 }
