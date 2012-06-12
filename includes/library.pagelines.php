54b8bc18645303a18f92b3338615d73b9df99d1c
Simon Prosser <simon@pagelines.com>
2012-06-12 16:41:44 +0100

    fix...
    Signed-off-by: Simon Prosser <simon@pagelines.com>

--- includes/library.pagelines.php
+++ includes/library.pagelines.php
@@ -1,5 +1,4 @@
 <?php
 if( !defined('VPRO' ) )
 	define( 'VPRO', true );
-if( is_admin() && current_user_can( 'edit_themes' ) )
-	@ini_set('memory_limit', WP_MAX_MEMORY_LIMIT );
+@ini_set('memory_limit', WP_MAX_MEMORY_LIMIT );
