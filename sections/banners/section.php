ea0dfb888dbf41ee037b8685461ce2dc73a61f0d
Simon Prosser <simon@pagelines.com>
2012-10-15 20:30:54 +0100

    Fix banner bug
    Signed-off-by: Simon Prosser <simon@pagelines.com>

--- sections/banners/section.php
+++ sections/banners/section.php
@@ -276,10 +276,9 @@ class PageLinesBanners extends PageLinesSection {
 	*
 	*/
 	function _get_banner_media( $oset ){
-		
 			
 			if(plmeta('the_banner_image', $oset))
-				$banner_media = '<img src="'.plmeta('the_banner_image', $oset).'" alt="'.get_the_title().'" />';
+				$banner_media = '<img src="'.plmeta('the_banner_image', $oset).'" alt="'.get_the_title( $oset['post_id'] ).'" />';
 			elseif(plmeta('the_banner_media', $oset))
 				$banner_media = do_shortcode( plmeta('the_banner_media', $oset) );
 			else 
