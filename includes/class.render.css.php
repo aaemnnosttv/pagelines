0e96e086a427d8bc47ab6a48121800c46fff7f17
Simon Prosser <simon@pagelines.com>
2012-11-27 00:49:18 +0000

    LESS FILE MODE by default
    Signed-off-by: Simon Prosser <simon@pagelines.com>

--- includes/class.render.css.php
+++ includes/class.render.css.php
@@ -76,10 +76,7 @@ class PageLinesRenderCSS {
 				
 		add_filter( 'query_vars', array( &$this, 'pagelines_add_trigger' ) );
 		add_action( 'template_redirect', array( &$this, 'pagelines_less_trigger' ) , 15);
-		
-		if( defined( 'LESS_FILE_MODE' ) && LESS_FILE_MODE )
-			add_action( 'template_redirect', array( &$this, 'less_file_mode' ) );
-
+		add_action( 'template_redirect', array( &$this, 'less_file_mode' ) );
 		add_action( 'wp_print_styles', array( &$this, 'load_less_css' ), 11 );
 		add_action( 'pagelines_head_last', array( &$this, 'draw_inline_custom_css' ) , 25 );
 		add_action( 'wp_head', array( &$pagelines_template, 'print_template_section_head' ), 12 );
@@ -97,6 +94,9 @@ class PageLinesRenderCSS {
 		if ( ! get_theme_mod( 'pl_save_version' ) )
 			return;
 
+
+		if( defined( 'LESS_FILE_MODE' ) && false == LESS_FILE_MODE )
+			return;
 
 		$folder = $this->get_uploads_folder();
 
