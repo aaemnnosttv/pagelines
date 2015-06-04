4b1c293de97b3029e433ca7475661bba0ed3cc66
Evan Mattson <me@aaemnnost.tv>
2015-06-03 22:50:04 -0400

    extend default parsed file headers to include PageLines
    for plugins & themes

--- includes/library.pagelines.php
+++ includes/library.pagelines.php
@@ -7,3 +7,10 @@ add_action('pagelines_max_mem', function() {
 	@ini_set('memory_limit', WP_MAX_MEMORY_LIMIT);
 });
 
+
+function pl_extend_default_headers( $extras ) {
+	$extras['PageLines'] = 'PageLines';
+	return $extras;
+}
+add_filter('extra_plugin_headers', 'pl_extend_default_headers');
+add_filter('extra_theme_headers', 'pl_extend_default_headers');
