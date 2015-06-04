8e68b966e1e81f621cc9627b2a7e43f958eacd6d
Evan Mattson <me@aaemnnost.tv>
2015-06-03 22:49:13 -0400

    convert create_function to closure

--- includes/library.pagelines.php
+++ includes/library.pagelines.php
@@ -1,4 +1,9 @@
 <?php
+
 if( !defined('VPRO' ) )
 	define( 'VPRO', true );
-add_action( 'pagelines_max_mem', create_function('',"@ini_set('memory_limit',WP_MAX_MEMORY_LIMIT);") );
+
+add_action('pagelines_max_mem', function() {
+	@ini_set('memory_limit', WP_MAX_MEMORY_LIMIT);
+});
+
